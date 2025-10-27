7e9670114d8c52724c358d258b5707ac95055f2e
nilsdnz <46533675+nilsdnz@users.noreply.github.com>
2025-10-27 08:56:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# WordPress Template
+
+## Setup Script
+
+```bash
+#!/bin/bash
+
+echo "WordPress Setup Script"
+
+read -p "App Name: " app
+read -p "App Domain: " domain
+read -p "Lets Encrypt Email: " ssl_email
+
+if [[ -n "$app" && -n "$domain" ]]
+then
+  echo "Creating dokku app $app..."
+  dokku apps:create $app
+
+  echo "Setting domain $domain..."
+  dokku domains:set $app $domain
+
+  echo "Setting up persistent storage..."
+  dokku storage:ensure-directory $app
+  sudo mkdir -p /var/lib/dokku/data/storage/${app}/wp-content
+  sudo mkdir -p /var/lib/dokku/data/storage/${app}/wp-content/plugins
+  sudo mkdir -p /var/lib/dokku/data/storage/${app}/wp-content/themes
+  sudo chown -R 32767:32767 /var/lib/dokku/data/storage/${app}
+  dokku storage:mount $app /var/lib/dokku/data/storage/$app/wp-content:/app/web/wp-content
+
+  echo "Creating the database..."
+  dokku mariadb:create $app-db --memory 2048 -C "TZ=Europe/Berlin"
+
+  echo "Linking the database..."
+  dokku mariadb:link $app-db $app
+
+  echo "Setting environment variables..."
+  dokku config:set $app WEB_HOST=https://$domain --no-restart
+  curl -sS "https://api.wordpress.org/secret-key/1.1/salt/" | awk -F"'" '{printf "WP_%s=\047%s\047 ", $2, $4}' | xargs dokku config:set $app --no-restart
+
+  echo "Configuring Nginx..."
+  dokku nginx:set $app client-max-body-size 512m
+
+  echo "Setting up Lets encrypt..."
+  dokku letsencrypt:set $app email $ssl_email
+  dokku letsencrypt:enable $app
+
+  echo "Finished WordPress setup!"
+fi
+```
---
+++ web/wp-config.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+/** Disable automatic updates */
+define('AUTOMATIC_UPDATER_DISABLED', true);
+
+$web_host = getenv('WEB_HOST');
+
+/** Site Address (URL) */
+define('WP_HOME', $web_host);
+
+/** WordPress Address (URL) */
+define('WP_SITEURL', $web_host);
+
+// ** Database settings ** //
+
+$db_url = parse_url(getenv('DATABASE_URL'));
+$db_host = $db_url['host'];
+$db_username = $db_url['user'];
+$db_password = $db_url['pass'];
+$db_database = substr($db_url['path'], 1);
+
+/** The name of the database for WordPress */
+define('DB_NAME', $db_database);
+
+/** Database username */
+define('DB_USER', $db_username);
+
+/** Database password */
+define('DB_PASSWORD', $db_password);
+
+/** Database hostname */
+define('DB_HOST', $db_host);
+
+/** Database charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The database collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', getenv('WP_AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('WP_SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('WP_LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('WP_NONCE_KEY'));
+define('AUTH_SALT', getenv('WP_AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('WP_LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('WP_NONCE_SALT'));
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define('WP_DEBUG', false);
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+define('FORCE_SSL_ADMIN', true);
+
+// in some setups HTTP_X_FORWARDED_PROTO might contain
+// a comma-separated list e.g. http,https
+// so check for https existence
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
